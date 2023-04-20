2ca6513458214824b4fcc00bb1961e61dfa83e23
Kenia <keniacunha100@gmail.com>
2023-04-19 23:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_freecipes.Models
+{
+    [Table ("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+
+}
