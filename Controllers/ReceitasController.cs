fb532f1490ef7c04b61f6f85dca4160962add903
GeovaneVLG <61693060+GeovaneVLG@users.noreply.github.com>
2023-04-27 11:07:47 -0300

    Update ReceitasController.cs

--- Controllers/ReceitasController.cs
+++ Controllers/ReceitasController.cs
@@ -1,10 +1,9 @@
-﻿using backend_freecipes.Models;
+using backend_freecipes.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace backend_freecipes.Controllers
+namespace mf_apis_web_services_fuel_manager.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
@@ -17,25 +16,59 @@ namespace backend_freecipes.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
             var model = await _context.Receitas.ToListAsync();
-
             return Ok(model);
         }
-
         [HttpPost]
         public async Task<ActionResult> Create(Receita model)
         {
- 
+           
             _context.Receitas.Add(model);
             await _context.SaveChangesAsync();
 
-            //return Ok(model);
             return CreatedAtAction("GetById", new { id = model.Id }, model);
         }
-    }
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Receitas
+                .Include(t => t.Usuario)
+                .FirstOrDefaultAsync(c => c.Id == id);
 
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Receita model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modeloDb = await _context.Receitas.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modeloDb == null) return NotFound();
+
+            _context.Receitas.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Receitas.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.Receitas.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
 }
